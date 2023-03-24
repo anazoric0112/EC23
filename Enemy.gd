fa1ce3d60c6fe4e5d9e0b3104ef3beaa6ec86f4a
LuTheDog <lukamatalzic@gmail.com>
2023-03-24 21:32:35 +0100

    Fixed enemy shooting

--- Enemy.gd
+++ Enemy.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 class_name Enemy
 
 const Projectile = preload("res://Projectile.tscn")
+const PROJECTILE_DIST = 40
 var player
 
 func _ready():
@@ -11,8 +12,10 @@ func shoot(target_position):
 	var projectile = Projectile.instance()
 	get_parent().add_child(projectile)
 	var projectile_direction = target_position - global_position
+	projectile_direction = projectile_direction.normalized()
 	projectile.direction = projectile_direction
+	projectile.global_position = global_position + projectile_direction * PROJECTILE_DIST
 
 
 func _on_Timer_timeout():
-	shoot(player)
+	shoot(player.global_position)
